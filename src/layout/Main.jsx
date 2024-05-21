3bbb1fa7e7ec124019b8f4c9bdb4e285d0698b01
Barshon <barshonweb@gmail.com>
2024-05-21 09:51:32 +0600

    shared components folder

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '../components/shared/Header';
+import Footer from '../components/shared/Footer';
 
 const Main = () => {
     return (
