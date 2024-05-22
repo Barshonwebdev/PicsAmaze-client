78294b267c0fbe9fa0fc86e1c36f7027c75776d7
Barshon <barshonweb@gmail.com>
2024-05-22 18:42:12 +0600

    user info from onauthstatechanged

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,15 +1,28 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import { createContext } from "react";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
 export const AuthContext=createContext('null');
 const auth=getAuth(app);
 const AuthProvider = ({children}) => {
+    const [user,setUser]=useState([]);
+
     const googleProvider= new GoogleAuthProvider();
     const googleLogin=()=>{
         return signInWithPopup(auth,googleProvider);
     }
 
-    const authInfo={googleLogin}
+    useEffect(()=>{
+        const unsubscribe= onAuthStateChanged(auth,currentUser=>{
+            if(currentUser){
+                setUser(currentUser);
+                console.log(currentUser);
+            }
+        })
+        return ()=>{
+            return unsubscribe();
+        }
+    },[])
+    const authInfo={googleLogin,user}
     return (
         <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
     );
