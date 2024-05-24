8fc8ea91d8516d8391c183db1000718a7e2415e3
Barshon <barshonweb@gmail.com>
2024-05-24 10:12:02 +0600

    user conditional email showing

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 const Header = () => {
   const { user, userLogout } = useAuth();
+  const [userState,setUserState]=useState(null);
   const handleLogout = () => {
     userLogout();
   };
   return (
     <div className="flex justify-end">
       <div className="lg:mr-5 flex flex-col items-center">
-        <p>logged in as: {user.email} </p>
+        {
+            user.email&&<p>logged in as: {user.email} </p>
+        }
 
         <div>
         <button
